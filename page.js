5db3db03c922ac31ae7127938b25a9941a976dc8
Kyle <kyle@unitedstudios.co.uk>
2018-10-30 17:03:52 +0000

    added removeTask functionality

--- page.js
+++ page.js
@@ -4,6 +4,7 @@
         var storage = chrome.storage.sync;
 
 
+
         // Refresh Tasks List
 
             function refresh_tasks_list(){
@@ -13,9 +14,14 @@
                     $('ul.task-list').empty();
 
                     if(result.tasks.length){
-                        result.tasks.forEach(function(task,ind){
-                            $('ul.task-list').append('<li>'+task.name+'</li>')
+                        result.tasks.forEach(function(task,ind){console.log(task)
+                            var li = '<li class="item" data-index="'+ind+'"><span>'+task.name+'</span><button class="btn-remove">X</button></li>';
+                            $('ul.task-list').append($(li))
                         });
+                        $('ul.task-list .btn-remove').off().on('click',function(){
+                            var index = parseInt($(this).closest('.item').attr('data-index'));
+                            removeTask(index);
+                        })
                     }
                 });
             }
@@ -28,10 +34,7 @@
             var setTask = function(new_task){
 
                 storage.get(['tasks'],function(result){
-
-                    var count = result.tasks.length;
-                    result.tasks[count] = new_task;
-
+                    result.tasks.push(new_task);
                     storage.set({'tasks':result.tasks},function(){
                         refresh_tasks_list();
                     });
@@ -52,14 +55,26 @@
                     &&  deadline.length
                 ){
                     setTask({
-                        task: name,
+                        name: name,
                         deadline: deadline
                     });
                 }
             });
 
 
-        // Clear Tasks
+        // Remove Task
+
+            function removeTask(index){
+                storage.get(['tasks'],function(result){
+                    result.tasks.splice(index,1);
+                    storage.set({'tasks':result.tasks},function(){
+                        refresh_tasks_list();
+                    });
+                });
+            }
+
+
+        // Clear all tasks
 
             function clearTaskList(){
                 storage.set({'tasks':[]},function(){
