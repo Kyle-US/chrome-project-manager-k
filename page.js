85353f1086044df4153c1b07a24aceaf481951a3
Kyle <kyle@unitedstudios.co.uk>
2018-10-30 15:19:15 +0000

    Added task list functions

--- page.js
+++ page.js
@@ -1,2 +1,57 @@
+(function($){
+    $(document).ready(function(){
 
+            var storage = chrome.storage.sync;
+
+
+        // Refresh Tasks List
+
+            function refresh_tasks_list(){
+
+                storage.get(['tasks'],function(result){
+
+                    $('ul.task-list').empty();
+
+                    if(result.tasks.length){
+                        result.tasks.forEach(function(task,ind){
+                            $('ul.task-list').append('<li>'+task.name+'</li>')
+                        });
+                    }
+                });
+            }
+            refresh_tasks_list();
+
+
+        // Save Task
+
+            $('.task-form').on('submit',function(e){e.preventDefault();
+
+                var name = $(this).find('input[name=name]').val();
+                var deadline = $(this).find('input[name=deadline]').val();
+
+                if(
+                    name.length
+                &&  deadline.length
+                ){
+                    storage.get(['tasks'],function(result){
+
+                        var save_data = {
+                            task: name,
+                            deadline: deadline
+                        };
+
+                        var count = result.tasks.length;
+                        result.tasks[count] = save_data;
+
+                        storage.set({'tasks':result.tasks},function(){
+                            refresh_tasks_list();
+                        });
+
+                    });
+                }
+            })
+
+
+    })
+}(jQuery));
 
