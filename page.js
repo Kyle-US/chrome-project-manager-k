2defb4c8f2d1ea9b3becd506856fdfffc69789ac
Kyle <kyle@unitedstudios.co.uk>
2018-10-31 15:50:40 +0000

    style updates

--- page.js
+++ page.js
@@ -21,7 +21,7 @@
 
             function refresh_tasks_list(){
 
-                storage.get(['tasks'],function(result){
+                storage.get(['tasks'],function(result){console.log(result)
 
                     $('ul.task-list').empty();
 
@@ -29,11 +29,15 @@
                         result.tasks.forEach(function(task,ind){
 
                             var $li = $('<li class="task" data-index="'+ind+'"></li>');
+                            var $priority = $('<div class="priority priority-'+task.priority+'"></div>');
                             var $name = $('<div class="name">'+task.name+'</div>');
-                            var deadline = task.deadline === 'unset' ? '' : '<div class="deadline">'+task.deadline+'</div>';
-                            var $deadline = $(deadline);
+                            var $deadline = task.deadline === 'unset' ? $('') : $('<div class="deadline">Due: '+task.deadline+'</div>');
                             var $close_btn = $('<button class="btn-remove">delete</button>');
-                            var $task = $li.append($name,$deadline,$close_btn);
+
+                            var $task_details = $('<div class="details"></div>');
+                            $task_details.append($name,$deadline,$close_btn);
+
+                            var $task = $li.append($priority,$task_details);
 
                             $('ul.task-list').append($task)
                         });
@@ -69,24 +73,29 @@
 
                 var $name = $(this).find('input[name=name]');
                 var $deadline = $(this).find('input[name=deadline]');
-                var $priority = $(this).find('input[name=priority]');
+                var $priority = $(this).find('input[name=priority]:checked');
 
                 var name = $name.val();
+
                 var deadline = $deadline.val();
                 deadline = deadline ? deadline : 'unset';
+
+                var priority = $priority.val();
 
                 $name.val('');
                 $deadline.val('');
                 $priority.val('');
-
+                console.log(priority)
                 if(
                         name.length
                     &&  deadline.length
+                    &&  priority.length
                 ){
-                    $('.task-form').removeClass('active')
+                    $(this).removeClass('active');
                     setTask({
                         name: name,
-                        deadline: deadline
+                        deadline: deadline,
+                        priority: priority
                     });
                 }
             });
@@ -109,7 +118,6 @@
             $('.btn-add-task').on('click',function(){
                 $('.task-form').addClass('active')
             });
-
 
 
         // Clear all tasks
