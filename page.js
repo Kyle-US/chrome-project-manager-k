218bbf0e9974536abeb9d21491d4cd8f42ba4351
Kyle <kyle@unitedstudios.co.uk>
2018-11-01 10:01:50 +0000

    added reorder functionality

--- page.js
+++ page.js
@@ -21,48 +21,72 @@
 
             function refresh_tasks_list(){
 
-                storage.get(['tasks'],function(result){console.log(result)
+                storage.get(['tasks'],function(result){
 
                     $('ul.task-list').empty();
 
-                    if(result.tasks.length){var i=0;
-                        result.tasks.forEach(function(task,ind){i++;
+                    if(result.tasks.length){
 
-                            var $li = $('<li class="task" data-index="'+ind+'"></li>');
-                            var $priority = $('<div class="priority priority-'+task.priority+'"></div>');
-                            var $name = $('<div class="name">'+task.name+'</div>');
-                            var $deadline = task.deadline === 'unset' ? $('') : $('<div class="deadline">Due: '+task.deadline+'</div>');
-                            var $delete_btn = $('<button class="btn-remove">delete</button>');
+                        // reorder by priority
 
-                            var $colors;
-                            $colors = '<label class="color-1" data-priority="1"></label>';
-                            $colors += '<label class="color-2" data-priority="2"></label>';
-                            $colors += '<label class="color-3" data-priority="3"></label>';
-                            $colors += '<label class="color-4" data-priority="4"></label>';
-                            $colors += '<label class="color-5" data-priority="5"></label>';
+                        var priorities = [5,4,3,2,1];
+                        var ordered_tasks_arr = [];
 
-                            $priority_colors = $('<div class="priority-colors">'+$colors+'</div>').clone();
+                        priorities.forEach(function(p){
 
-                            var $task_details = $('<div class="details"></div>');
-                            $task_details.append($name,$deadline,$priority_colors,$delete_btn);
+                            result.tasks.forEach(function(task,ind){
 
-                            var $task = $li.append($priority,$task_details);
+                                if(p == task.priority){
+                                    ordered_tasks_arr.push(task);
+                                }
+                            })
 
-                            $('ul.task-list').append($task)
                         });
-                        $('ul.task-list .btn-remove').off().on('click',function(){
-                            var r = confirm("Delete this task forever?");
-                            if(r){
-                                var index = parseInt($(this).closest('.task').attr('data-index'));
-                                removeTask(index);
-                            }
-                        })
-                        $('ul.task-list .priority-colors label').off().on('click',function(){
 
-                            var task_index = $(this).closest('.task').attr('data-index');
-                            var priority = $(this).attr('data-priority');
-                            update_task(task_index,'priority',priority);
-                        })
+                        var tasks = ordered_tasks_arr;
+
+
+                        // reorder by priority
+
+                            var i=0;
+                            tasks.forEach(function(task,ind){ i++;
+
+                                var $li = $('<li class="task" data-index="'+ind+'"></li>');
+                                var $priority = $('<div class="priority priority-'+task.priority+'"></div>');
+                                var $name = $('<div class="name">'+task.name+'</div>');
+                                var $deadline = task.deadline === 'unset' ? $('') : $('<div class="deadline">Due: '+task.deadline+'</div>');
+                                var $delete_btn = $('<button class="btn-remove">delete</button>');
+
+                                var $colors;
+                                $colors = '<label class="color-1" data-priority="1"></label>';
+                                $colors += '<label class="color-2" data-priority="2"></label>';
+                                $colors += '<label class="color-3" data-priority="3"></label>';
+                                $colors += '<label class="color-4" data-priority="4"></label>';
+                                $colors += '<label class="color-5" data-priority="5"></label>';
+
+                                $priority_colors = $('<div class="priority-colors">'+$colors+'</div>').clone();
+
+                                var $task_details = $('<div class="details"></div>');
+                                $task_details.append($name,$deadline,$priority_colors,$delete_btn);
+
+                                var $task = $li.append($priority,$task_details);
+
+                                $('ul.task-list').append($task)
+                            });
+                            $('ul.task-list .btn-remove').off().on('click',function(){
+                                var r = confirm("Delete this task forever?");
+                                if(r){
+                                    var index = parseInt($(this).closest('.task').attr('data-index'));
+                                    removeTask(index);
+                                }
+                            });
+                            $('ul.task-list .priority-colors label').off().on('click',function(){
+
+                                var task_index = $(this).closest('.task').attr('data-index');
+                                var priority = $(this).attr('data-priority');
+                                update_task(task_index,'priority',priority);
+                            })
+
                     }
                 });
             }
